f6606b8db32eb9a2f9f561c4f18d9d618218b6f8
iamncla <blindmind6@gmail.com>
2014-06-01 14:48:44 +0300

    New feature: get my fucking inventory

--- csgl3000.js
+++ csgl3000.js
@@ -3,26 +3,26 @@
 // @namespace  http://csgolounge.com/
 // @version    0.1
 // @description  Spam the fuck out of the CS:GL queue system, because it's absolute crap
-// @match      http://csgolounge.com/match?m=*
+// @match      http://csgolounge.com/*
+// @match      http://dota2lounge.com/*
 // @copyright  iamncla @ GitHub.com
 // ==/UserScript==
 var Bet3000 = function(matchID) {
     /* Construct */
     var self = this;
     
-    this.ID = matchID;
     this.attempts = 0;
     
     this.itemsInReturn = returns;
     
     this.placeBet = function() {
-        if(!this.checkRequirements()) return false;
+        if(!this.checkBetRequirements()) return false;
         // returns variable is created by CS:GL page, true if you are using return items.
         var url = (returns == true ? "ajax/postBet.php" : "ajax/postBetOffer.php");
         $.ajax({
                 type: "POST",
                 url: url,
-                data: $("#betpoll").serialize() + "&match=" + self.ID,
+                data: $("#betpoll").serialize() + "&match=" + self.matchID,
                 success: function(data) {
                     if (data) {
                         self.attempts = self.attempts + 1;
@@ -35,7 +35,7 @@ var Bet3000 = function(matchID) {
                 }
             });
     }
-    this.checkRequirements = function() {
+    this.checkBetRequirements = function() {
         if(!$(".betpoll .item").length > 0) { 
             alert("No items added!");
             return false;
@@ -46,13 +46,43 @@ var Bet3000 = function(matchID) {
         }
         return true;
     }
+    this.getInventoryItems = function() {
+        if (typeof ChoseInventoryReturns == 'function') {
+            var basher = setInterval(function() {
+                if($("#backpack .standard").text().indexOf("Can't get items.") == -1) {
+                    clearInterval(basher);
+                    $("#showinventorypls").hide();
+                    return true;
+                }
+                var steamAPI = ((Math.floor(Math.random() * (1 - 0 + 1)) + 0) == 0 ? "betBackpackApi" : "betBackpack");
+                ChoseInventoryReturns(steamAPI);
+                console.log("Attempting to get your Steam inventory!");
+            }, 2000); // A little more gentle on bashing servers, because it's Volvo, not CS:GL
+        }
+    }
 }
+var Bet = new Bet3000();
+
 if($("#placebut").length) {
     $("#placebut").before("<a class='buttonright' id='realbetbutton'>FUCKING PLACE A BET</a>");
-    var Bet = new Bet3000(gup("m"));
+    Bet.matchID = gup("m");
     $("#realbetbutton").click(function() {
         Bet.placeBet();
     });
+}
+if($("#backpack").length) {
+    if($("#backpack #loading").length) {
+        // DOMSubtreeModified might have poor support
+        $("#backpack").bind("DOMSubtreeModified", function(event) {
+            if($("#backpack .standard").text().indexOf("Can't get items.") != -1) {
+                $("#backpack").unbind();
+                $("#backpack").before("<a class='buttonright' id='showinventorypls'>FUCKING GET MY INVENTORY</a>");
+                $("#showinventorypls").click(function() {
+                    Bet.getInventoryItems();
+                })
+            }
+        })
+    }
 }
 
 function gup(name) {
