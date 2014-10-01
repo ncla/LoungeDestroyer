72e5676fd98922fae13a3e1dabad007836c51700
birjolaxew <johanringmann@gmail.com>
2014-10-01 17:11:37 +0200

    Minor polish to autobetting popup

--- src/inject/app/bet.js
+++ src/inject/app/bet.js
@@ -4,7 +4,8 @@ var inventory = new Inventory();
 (function(){
 	if (window.location.pathname === "/match") {
 		var placebut;
-		if ((placebut = document.getElementById("placebut"))) {
+		if ((placebut = document.getElementById("placebut")) &&
+			placebut.getAttribute("onclick").indexOf("placeBetNew") !== -1) {
 			var newBtn = document.createElement("a");
 			newBtn.id = "realbetbutton";
 			newBtn.className = "buttonright";
@@ -22,13 +23,23 @@ var betStatus = {
 	rebetDelay: 10000
 };
 
-function enableAuto(worth) {
+function enableAuto(worth, match, tries, error) {
 	console.log("Enabling auto");
 	betStatus.enabled = true;
+
+	var ordinalEnding = ((tries||0)+"").slice(-1);
+	ordinalEnding = (ordinalEnding === "1" ? "st":
+		             ordinalEnding === "2" ? "nd":
+		             ordinalEnding === "3" ? "rd":
+		             "th");
 
 	// update info-box in top-right
     document.querySelector(".destroyer.auto-info").className = "destroyer auto-info";
     document.querySelector(".destroyer.auto-info .worth").textContent = "$"+(worth || 0).toFixed(2);
+    document.querySelector(".destroyer.auto-info .match-link").textContent = match;
+    document.querySelector(".destroyer.auto-info .match-link").href = "http://csgolounge.com/match?m="+match;
+    document.querySelector(".destroyer.auto-info .num-tries").textContent = tries + ordinalEnding;
+    document.querySelector(".destroyer.auto-info .error-text").textContent = error;
     document.getElementById("bet-time").valueAsNumber = betStatus.rebetDelay / 1000;
 
     // update timer
@@ -57,7 +68,7 @@ chrome.runtime.sendMessage({get: "autoBet"}, function(data){
 	betStatus.betTime = data.time;
 	betStatus.rebetDelay = data.rebetDelay;
 	betStatus.enabled = true;
-	enableAuto(data.worth);
+	enableAuto(data.worth, data.matchId, data.numTries, data.error);
 });
 
 // listen for auto-betting updates
@@ -84,16 +95,19 @@ chrome.runtime.onMessage.addListener(function(request,sender,sendResponse){
 	}
 
 	if (request.autoBet) {
-		if (request.autoBet.worth && request.autoBet.time && request.autoBet.rebetDelay) { // autobetting has started
+		// autobetting has started
+		if (request.autoBet.worth && request.autoBet.time && request.autoBet.rebetDelay) {
+			console.log(request.autoBet);
 			betStatus.enabled = true;
 			betStatus.betTime = request.autoBet.time;
 			betStatus.rebetDelay = request.autoBet.rebetDelay;
 
-			enableAuto(request.autoBet.worth);
+			enableAuto(request.autoBet.worth, request.autoBet.matchId, request.autoBet.numTries, request.autoBet.error);
 			return;
 		}
 
-		if (request.autoBet.time && request.autoBet.error) { // autobetting has received an error from Lounge
+		// autobetting has received an error from Lounge
+		if (request.autoBet.time && request.autoBet.error) {
 			document.querySelector(".destroyer.auto-info .error-text").textContent = request.autoBet.error;
 			betStatus.betTime = request.autoBet.time;
 			return;
@@ -147,6 +161,8 @@ function onAutobetClicked() {
 	var container = document.createElement("div"),
 	    paragraphs = [document.createElement("p"),document.createElement("p"),document.createElement("p")],
 	    worthSpan = document.createElement("span"),
+	    matchLink = document.createElement("a"),
+	    triesSpan = document.createElement("span"),
 	    btn = document.createElement("button"),
 	    timeSpan = document.createElement("span"),
 	    label = document.createElement("label"),
@@ -162,6 +178,9 @@ function onAutobetClicked() {
 	paragraphs[1].className = "destroyer error-title";
 	paragraphs[2].className = "destroyer error-text";
 	worthSpan.className = "worth";
+	matchLink.className = "match-link";
+	triesSpan.className = "num-tries";
+	triesSpan.textContent = "0th"
 	btn.className = "red";
 	btn.textContent = "Disable auto-bet";
 	label.textContent = "Seconds between retries:";
@@ -174,11 +193,17 @@ function onAutobetClicked() {
 	btn.addEventListener("click", function(){
 		chrome.runtime.sendMessage({"autoBet": false});
 	});
-	betTime.addEventListener("change", function(){
-		chrome.runtime.sendMessage({"set": {bet: {autoDelay: this.valueAsNumber * 1000}}});
+	betTime.addEventListener("input", function(){
+		if (this.valueAsNumber)
+			chrome.runtime.sendMessage({"set": {bet: {autoDelay: this.valueAsNumber * 1000}}});
 	}); // TO-DO: save setting
 
 	paragraphs[0].appendChild(worthSpan);
+	paragraphs[0].appendChild(document.createTextNode(" on match "));
+	paragraphs[0].appendChild(matchLink);
+	paragraphs[0].appendChild(document.createTextNode(". Betting for the "));
+	paragraphs[0].appendChild(triesSpan);
+	paragraphs[0].appendChild(document.createTextNode(" time."));
 
 	container.appendChild(paragraphs[0]);
 	container.appendChild(btn);
