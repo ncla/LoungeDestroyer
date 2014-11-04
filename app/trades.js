352a9a387b45f2d1d4a41864d7b18fe998f681b5
iamncla <blindmind6@gmail.com>
2014-11-04 17:03:14 +0200

    Correct detection for converted price

---
+++ app/trades.js
@@ -0,0 +1,23 @@
+var Trade = function(tradeElement) {
+    var self = this;
+    this.tradeElement = tradeElement;
+
+    var tradeAnchor = $("a[href^=\"trade?\"]:eq(0)", self.tradeElement);
+    if($(tradeAnchor).length) {
+        console.log("Anchor found");
+        this.tradeID = tradeAnchor.attr("href").match(/\d+$/)[0] || false;
+    } else {
+        console.log("Anchor not found");
+    }
+
+    //console.log(self.tradeID.match(/\d+$/)[0]);
+};
+Trade.prototype.generateTradeURL = function() {
+    return;
+};
+$(document).ready(function() {
+    $(".tradepoll").each(function(i, v) {
+        var derpTest = new Trade($(v));
+        console.log(derpTest);
+    });
+});
