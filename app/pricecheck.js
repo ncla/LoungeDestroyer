4afc463c779b2950b2c7a6a3879b52cbc79fbf38
iamncla <blindmind6@gmail.com>
2015-02-21 20:43:02 +0200

    Fixed one more time invalid item names on Steam market listings for price checks when using non-english language

--- app/pricecheck.js
+++ app/pricecheck.js
@@ -1,10 +1,40 @@
-var itemName = $("div.market_listing_nav:eq(0) a:last-child").text().trim() || false;
+var itemName = false;
+
+// This is basically to get original item name from g_rgAssets variable, if all goes well the itemName should be appended
+// to the body element with attribute 'data-itemname-ld'
+function scriptInject () {
+    try {
+        for(var prop in g_rgAssets) {
+            var appIDprop = g_rgAssets[prop];
+            break;
+        }
+        for(var inventory in appIDprop) {
+            var inventoryProp = appIDprop[inventory];
+            break;
+        }
+        for(var asset in inventoryProp) {
+            var firstAsset = inventoryProp[asset];
+            if(firstAsset.hasOwnProperty('market_hash_name')) {
+                var marketHashName = firstAsset['market_hash_name'];
+                document.body.setAttribute('data-itemname-ld', marketHashName);
+            }
+            break;
+        }
+    } catch(e) {}
+}
+// Injecting script
+addJS_Node(null, null, scriptInject, null);
+
+function getItemName() {
+    return $("body").attr("data-itemname-ld") || $("div.market_listing_nav:eq(0) a:last-child").text() || false;
+}
 
 var LoungeUser = new User();
 chrome.storage.local.get(['currencyConversionRates', 'ajaxCache'], function(result) {
     currencies = result.currencyConversionRates || {};
     ajaxCache = result.ajaxCache || {};
     LoungeUser.loadUserSettings(function() {
+        itemName = getItemName();
         if(itemName) {
             $("#largeiteminfo_item_actions").append('<span class="btn_small btn_grey_white_innerfade" id="csglpricecheck">' +
                 '<span>Check CSGOLounge.com item betting value</span>' +
