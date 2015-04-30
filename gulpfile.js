0b1923809f9584ce9f516c1c739d8664824ed353
ncla <blindmind6@gmail.com>
2015-04-30 14:20:06 +0300

    Removed old icons, added build command for Gulp that will minify code and archive it into a zip

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,44 @@
 var gulp = require('gulp');
 var jscs = require('gulp-jscs');
+var gulpFilter = require('gulp-filter');
+var uglify = require('gulp-uglify');
+var minifyCss = require('gulp-minify-css');
+var minifyHTML = require('gulp-minify-html');
+var zip = require('gulp-zip');
 
 gulp.task('default', function () {
     return gulp.src('app/*.js')
         .pipe(jscs());
+});
+
+var requiredStuff = [
+    'app/**/*.{js,css}',
+    'icons/*',
+    'lib/css/bootstrap.min.css',
+    'lib/css/ld.css',
+    'lib/css/font-awesome.min.css',
+    'lib/fonts/*',
+    'lib/js/*',
+    'popup/*',
+    'settings/**/*',
+    'manifest.json'
+];
+
+gulp.task('build', function () {
+    var jsFilter = gulpFilter(['**/*.js']);
+    var cssFilter = gulpFilter(['**/*.css']);
+    var htmlFilter = gulpFilter(['**/*.html']);
+
+    return gulp.src(requiredStuff, {base: '.'})
+        .pipe(jsFilter)
+        .pipe(uglify())
+        .pipe(jsFilter.restore())
+        .pipe(cssFilter)
+        .pipe(minifyCss())
+        .pipe(cssFilter.restore())
+        .pipe(htmlFilter)
+        .pipe(minifyHTML())
+        .pipe(htmlFilter.restore())
+        .pipe(zip('ld_build.zip'))
+        .pipe(gulp.dest('build/'));
 });
